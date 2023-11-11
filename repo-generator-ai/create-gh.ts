f5fb901ee0b995f92e7b862f9369ad8c0a973f3f
Rodrigo Faria <rodrigo.faria@cartrack.com>
2023-11-11 17:13:24 +0000

    cenas

--- repo-generator-ai/create-gh.ts
+++ repo-generator-ai/create-gh.ts
@@ -2,16 +2,24 @@ const prompt = require("prompt-sync")();
 const { Octokit } = require("@octokit/rest");
 const { execSync } = require("child_process");
 const fs = require("fs");
+const { join } = require("path");
+
+type Credentials = {
+  username: string;
+  token: string;
+};
 
 // Function to get GitHub credentials from the user
-function getGitHubCredentials() {
-  const username = prompt("Enter your GitHub username: ");
-  const token = prompt.hide("Enter your GitHub personal access token: "); // Use prompt.hide to hide the token input
+function getGitHubCredentials(): Credentials {
+  // const username = prompt("Enter your GitHub username: ");
+  const username = "rodrigofariow";
+  // const token = prompt.hide("Enter your GitHub personal access token: "); // Use prompt.hide to hide the token input
+  const token = "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";
   return { username, token };
 }
 
 // Function to create a new GitHub repository
-async function createRepository(credentials, repoName) {
+async function createRepository(credentials: Credentials, repoName: string) {
   try {
     const octokit = new Octokit({
       auth: credentials.token,
@@ -29,7 +37,7 @@ async function createRepository(credentials, repoName) {
 }
 
 // Function to initialize a local git repository
-function initializeLocalRepo(repoPath) {
+function initializeLocalRepo(repoPath: string) {
   try {
     execSync(`git init "${repoPath}"`);
     console.log("Local repository initialized successfully.");
@@ -39,7 +47,7 @@ function initializeLocalRepo(repoPath) {
 }
 
 // Function to add and commit local changes
-function addAndCommitChanges(repoPath) {
+function addAndCommitChanges(repoPath: string) {
   try {
     execSync(`cd "${repoPath}" && git add .`);
     execSync(`cd "${repoPath}" && git commit -m "Initial commit"`);
@@ -50,10 +58,10 @@ function addAndCommitChanges(repoPath) {
 }
 
 // Function to push changes to the GitHub repository
-function pushToGitHub(credentials, repoName, repoPath) {
+function pushToGitHub(credentials: Credentials, repoName: string, repoPath: string) {
   try {
     execSync(
-      `cd "${repoPath}" && git remote add origin https://${credentials.username}:${credentials.token}@github.com/${credentials.username}/${repoName}.git`
+      `cd "${repoPath}" && git remote add origin https://${credentials.username}:${credentials.token}@github.com/${credentials.username}/${repoName}.git`,
     );
     execSync(`cd "${repoPath}" && git push -u origin master`);
     console.log("Code pushed to GitHub repository successfully.");
@@ -68,19 +76,30 @@ async function main() {
     // Get user input
     const credentials = getGitHubCredentials();
     const repoName = prompt("Enter the name for the new GitHub repository: ");
-    const repoPath = prompt("Enter the local path of the code to push: ");
+    // const repoPath = prompt("Enter the local path of the code to push: ");
+
+    execSync(
+      `cp -r ${join(__dirname, "preset_templates/*")} ${join(
+        __dirname,
+        "user_templates",
+      )}`,
+    );
+    const userTemplateAbsolutePath = join(__dirname, "user_templates");
 
     // Create GitHub repository
     await createRepository(credentials, repoName);
 
     // Initialize local git repository
-    initializeLocalRepo(repoPath);
+    initializeLocalRepo(userTemplateAbsolutePath);
 
     // Add and commit local changes
-    addAndCommitChanges(repoPath);
+    addAndCommitChanges(userTemplateAbsolutePath);
 
     // Push changes to GitHub repository
-    pushToGitHub(credentials, repoName, repoPath);
+    pushToGitHub(credentials, repoName, userTemplateAbsolutePath);
+
+    execSync(`rm -rf ${userTemplateAbsolutePath}/*`);
+    execSync(`rm -rf ${userTemplateAbsolutePath}/.git`);
   } catch (error) {
     console.error("An unexpected error occurred:", error.message);
   }
